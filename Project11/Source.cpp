32e999b1a2c893ef9f84a708ec3cacc91b1cd6d9
Robert Carmelitano <rcarmelitano@gmail.com>
2017-12-16 18:57:10 -0500

    Prototype that works with 5 numbers. Still need to add more  math functions and let them pick the size of the array.

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <Windows.h>
 using namespace std;
 
 //Globals
 const int NUM_INPUTS = 5;
 int inputNumbers[NUM_INPUTS];
 //Function Headers
+
+//Clears the terminal
+void ClearTerminal()
+{
+	system("cls");
+}
+//Gets X numbers from the user
 void GetNumbers()
 {
 	//Strings
 	const string INTRO_PROMPT = "Enter five numbers to generate a report.";
 	const string ENTER_NUM_PROMPT = "Enter a number:";
-	cout << INTRO_PROMPT;
+	cout << INTRO_PROMPT << endl;
 	for (int i = 0; i < NUM_INPUTS; i++)
 	{
 		cout << ENTER_NUM_PROMPT;
 		cin >> inputNumbers[i];
 	}
-	cout << ENTER_NUM_PROMPT;
+	
+}
+//Returns the average of the contents of the array
+int AverageNumbers()
+{
+	//Holds the average of the array
+	int arrayAverage = 0;
+	int arraySum = 0;
+	//Calculates sum and then average
+	for (int i = 0; i < NUM_INPUTS; i++)
+	{
+		arraySum = arraySum + inputNumbers[i];
+		if (i == (NUM_INPUTS-1))
+		{
+			arrayAverage = arraySum / NUM_INPUTS;
+		}
+	}
+	return arrayAverage;
+
+}
+
+int main()
+{
+	//Strings
+	string AVG_PROMPT = "The average of your numbers is ";
+	//Get the numbers
+	GetNumbers();
+	//Average the numbers
+	cout << AVG_PROMPT << AverageNumbers() << endl;
+	//Pause
+	system("pause");
+	return 0;
 }
