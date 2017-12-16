ed1eb8c76d5d9152917f64d79a966470ae440ca9
Robert Carmelitano <rcarmelitano@gmail.com>
2017-12-16 18:35:41 -0500

    Building a getnumbers function

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
+using namespace std;
+
+//Globals
+const int NUM_INPUTS = 5;
+int inputNumbers[NUM_INPUTS];
+//Function Headers
+void GetNumbers()
+{
+	//Strings
+	const string INTRO_PROMPT = "Enter five numbers to generate a report.";
+	const string ENTER_NUM_PROMPT = "Enter a number:";
+	cout << INTRO_PROMPT;
+	for (int i = 0; i < NUM_INPUTS; i++)
+	{
+		cout << ENTER_NUM_PROMPT;
+		cin >> inputNumbers[i];
+	}
+	cout << ENTER_NUM_PROMPT;
+}
